cadff7efceceb808031da14ca10c112ba8d31d2f
twfce <me@2fce.de>
2022-02-20 11:18:36 +0100

    New site

--- seitleio/cache/compiled/files/3046b40f21e878fe90fb28dc12857db3.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/var/www/html/user/config/security.yaml',
-    'modified' => 1643959431,
-    'data' => [
-        'salt' => 'CGIrke91oLJZ20'
-    ]
-];
